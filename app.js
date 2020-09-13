a7b9f95fbf97f194082db10cc98f09c37e1bde0a
ekaterina-barysheva <barysheva.ek@gmail.com>
2020-09-13 12:48:12 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,48 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+const app = express();
+
+const items = [];
+const workItems = [];
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+app.get("/", function(req, res) {
+
+  let day = date.getDate();
+  res.render("list", {
+    listTitle: day,
+    newListItems: items
+  });
+});
+app.get("/about", function(req, res) {
+  res.render("about");
+});
+
+app.get("/work", function(req, res) {
+  res.render("list", {
+    listTitle: "Work",
+    newListItems: workItems
+  });
+});
+app.post("/", function(req, res) {
+  let item = req.body.newItem;
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000.");
+});
